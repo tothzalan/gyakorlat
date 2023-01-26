a33a54e551f869900bd8cbfd2eb7707f744af1cf
tothzalan <toth.zalan03@gmail.com>
2023-01-26 20:07:31 +0100

    removed get_tag function

--- indexer.py
+++ indexer.py
@@ -6,7 +6,8 @@ def web_request(url: str) -> str:
     response = requests.get(url)
     if response.status_code == 200:
         return response.text
-    raise Exception(f"{url} returned with code {response.status_code}")
+    #raise Exception(f"{url} returned with code {response.status_code}")
+    return ""
 
 class Company:
     def __init__(self, name, url):
@@ -29,14 +30,6 @@ technologies = [ "python", "django", "php", "laravel",
                  "angular", "react", "golang", "scala", "unity",
                  "docker", "lua"]
 technologies_pattern = re.compile(f" ({'|'.join(technologies)})", flags=re.I)
-
-def get_tag(tag: str, input: str) -> str:
-    tags = []
-    for line in input.split("\n"):
-        if tag in line:
-            tags.append(line)
-    return tags
-
 def parse_link(raw: str) -> Company:
     name = re.search("\">.*\<\/a", raw).group(0)[2:-3]
     link = re.search("\".*\"", raw).group(0)[1:-1]
@@ -44,9 +37,10 @@ def parse_link(raw: str) -> Company:
 
 if __name__ == "__main__":
     BASE_URL = "https://www.inf.elte.hu/bsc-kepzes/szakmai-gyakorlati-helyek"
-    response_text = web_request(BASE_URL)
-    links = get_tag("<li><a", response_text)
-    companies = [parse_link(l) for l in links]
-    for i in range(3):
-        companies[i].gather_tech()
-        print(companies[i])
+
+    soup = BeautifulSoup(web_request(BASE_URL), features="html.parser")
+    container_ul = soup.find_all("ul")[-1]
+    links = list(container_ul.findChildren("a", recursive=True))
+    companies = [parse_link(str(l)) for l in links][:3]
+    [c.gather_tech() for c in companies]
+    [print(c) for c in companies]
