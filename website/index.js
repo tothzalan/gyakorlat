c55a3681158ca27439b0884c5f511a54f94b7d5b
tothzalan <toth.zalan03@gmail.com>
2023-02-02 16:28:38 +0100

    basic search

--- website/index.js
+++ website/index.js
@@ -1,25 +1,43 @@
 
-const dummyData = { 'companies': [
-    { 'name': 'TesztSoft', 'url': 'https://google.com', technologies: [ 'Go', 'C++'] },
+let dummyData = { 'companies': [
+    { 'name': 'TesztSoft', 'url': 'https://google.com', technologies: [ 'Go', 'C++', 'PHP'] },
     { 'name': 'MasikSoft', 'url': 'https://facebook.com', technologies: [ 'PHP', 'React'] },
 ]}
 
 const container = document.getElementById('container')
 
-dummyData.companies.map(comp => {
-    const div = document.createElement('div')
 
-    const a = document.createElement('a')
-    a.innerText = comp.name
-    a.href = comp.url
-    a.target = '_blank'
-    div.appendChild(a)
+function showData(data) {
+    container.innerHTML = ''
+    data.companies.map(comp => {
+        const div = document.createElement('div')
 
-    const p = document.createElement('p')
-    comp.technologies.forEach(t => {
-        p.innerText += `${t} `
+        const a = document.createElement('a')
+        a.innerText = comp.name
+        a.href = comp.url
+        a.target = '_blank'
+        div.appendChild(a)
+
+        const p = document.createElement('p')
+        comp.technologies.forEach(t => {
+            p.innerText += `${t} `
+        })
+        div.appendChild(p)
+
+        container.appendChild(div)
     })
-    div.appendChild(p)
+}
+showData(dummyData)
 
-    container.appendChild(div)
-})
+
+function search() {
+    const selectedTech = document.getElementById('select').value
+    if(selectedTech) {
+        const filtered = {}
+        filtered.companies = dummyData.companies.filter(comp =>
+            comp.technologies.includes(selectedTech))
+        showData(filtered)
+    } else {
+        showData(dummyData)
+    }
+}
